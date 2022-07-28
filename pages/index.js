1ecadf83bf828dd169bb4247cf075e6836b399f5
agnieve70 <agnieve70@gmail.com>
2022-07-28 23:58:15 +0800

    added fixes

--- pages/index.js
+++ pages/index.js
@@ -21,8 +21,8 @@ export default function Home() {
         <h4 className="text-lg text-bold text-primary-1">School ID No. 466527</h4>
       </div>
 
-      <div className="px-20 pt-10 space-y-8">
-        <h1 className="text-primary-1  text-3xl">CHRISTIAN SCHOOL K to 12 CURRICULUM</h1>
+      <div className="px-20 md:px-10 md:bg-red pt-10 space-y-8">
+        <h1 className="text-primary-1 text-3xl">CHRISTIAN SCHOOL K to 12 CURRICULUM</h1>
         <p>
           Children who attend Christian schools have the same variety of personalities, talents, interests and abilities as children who attend public school.
           The difference between a Christian school and public school lies in the foundation of the curriculum and the way in which the
@@ -50,35 +50,6 @@ export default function Home() {
           <p>Proverbs 22:6 (KJV Bible)</p>
         </p>
       </div>
-
-      {/* <div className="px-20 pt-10 space-y-8">
-        <h1 className="text-primary-1 text-3xl">Mission</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-          imperdiet viverra ante et tincidunt. Nullam mollis, elit consectetur
-          iaculis tempor, ante augue accumsan odio, ut feugiat eros velit sed
-          nibh. Nam eu eros fermentum, aliquam sem at, accumsan augue. Aenean
-          lacus lorem, interdum vel fermentum sed, sodales id libero. Nullam
-          vulputate commodo urna id mattis. Fusce iaculis maximus felis. Nullam
-          et orci at mi ultricies laoreet et eget ipsum. Aliquam luctus posuere
-          efficitur. Mauris scelerisque leo malesuada tellus malesuada accumsan.
-          Ut non metus velit. Vestibulum lectus nulla, ornare a nisl nec,
-          pretium viverra tellus. Phasellus sit amet magna augue. Sed a diam
-          pulvinar metus auctor tincidunt sed quis purus. Donec id lorem diam.
-        </p>
-
-        <p>
-          {" "}
-          Donec eros risus, porttitor eget risus vel, pulvinar hendrerit ante.
-          Maecenas quis pretium metus. Vestibulum ante ipsum primis in faucibus
-          orci luctus et ultrices posuere cubilia curae; Nulla facilisi. Nam
-          rutrum, augue et sagittis aliquam, mi nisi lobortis enim, eget egestas
-          sapien urna in turpis. Quisque scelerisque dolor sagittis lorem
-          accumsan, at tempus odio convallis. Nulla venenatis pulvinar ipsum vel
-          auctor. Praesent a enim at erat fringilla lacinia. Aenean eu ultrices
-          ante.
-        </p>
-      </div> */}
     </div>
   );
 }
